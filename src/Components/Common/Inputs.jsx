d652f0aeda5286efc1ff348b33168d47b91a3230
qhahd78 <umuj0708@likelion.org>
2021-11-14 23:19:12 +0900

    [Setting][Feat]::글씨체 재설정, Join Page 퍼블리싱 준완료 -Umin

--- src/Components/Common/Inputs.jsx
+++ src/Components/Common/Inputs.jsx
@@ -1,23 +1,71 @@
 import React from "react";
 import styled from "styled-components";
+import PropTypes from "prop-types";
 import Colors from "../../Assets/Colors/Colors";
 
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
 const Input = styled.input`
-  font-family: "Pretendard-Bold";
-  border: 2px solid ${Colors.black};
+  font-family: "Pretendard Variable";
+  font-size: inherit;
+  box-sizing: border-box;
+  padding: 8px;
+  border: 1.5px solid ${Colors.black};
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  &::placeholder {
+    font-family: "Pretendard Variable";
+    font-weight: 300;
+  }
 `;
 
 const InputName = styled.span`
-  font-family: "Pretendard-Regular";
+  font-weight: 700;
+  font-size: 1.2rem;
+  display: inline-block;
+  width: 140px;
 `;
 
-const Inputs = () => {
+const Inputs = ({
+  inputType,
+  inputWidth,
+  inputHeight,
+  inputContent,
+  inputPlaceholder,
+}) => {
   return (
     <>
-      <InputName>이름</InputName>
-      <Input placeholder="안녕" />
+      <Container>
+        <InputName>{inputContent}</InputName>
+        <Input
+          type={inputType}
+          width={inputWidth}
+          height={inputHeight}
+          placeholder={inputPlaceholder}
+        />
+      </Container>
     </>
   );
+};
+
+Inputs.propTypes = {
+  inputType: PropTypes.string,
+  inputWidth: PropTypes.string,
+  inputHeight: PropTypes.string,
+  inputContent: PropTypes.string,
+  inputPlaceholder: PropTypes.string,
+};
+
+Inputs.defaultProps = {
+  inputType: "text",
+  inputWidth: "450px",
+  inputHeight: "45px",
+  inputContent: "None",
+  inputPlaceholder: "None",
 };
 
 export default Inputs;
---
+++ src/Pages/JoinPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Join from "../Layouts/Join/Join";
+
+const JoinPage = () => {
+  return <Join />;
+};
+
+export default JoinPage;
