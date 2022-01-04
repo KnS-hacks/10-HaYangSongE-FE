043414d16d40b626f82ae68d362220ffe2e33e71
qhahd78 <umuj0708@likelion.org>
2022-01-04 19:41:42 +0900

    [Fix]::CORS ERROR Fix git add .

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -4,24 +4,17 @@ Axios.defaults.baseURL = 'http://52.78.74.152';
 
 const api = ({
   url,
-  type = 'get',
+  type = 'GET',
   param,
   contentType = 'application/json',
 }) => {
-  const accessToken = sessionStorage.getItem('accessToken');
-
   const headers = {
     'Content-Type': contentType,
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH',
     'Access-Control-Allow-Headers':
       'Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization',
-    mode: 'cors',
   };
-
-  if (accessToken !== null) {
-    headers.Authorization = `Bearer ${accessToken}`;
-  }
 
   return Axios({
     method: type,
